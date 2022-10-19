f90f677207c486a534a2bb9129de42490ab9fee5
Colleen Stewart <collstewart30@gmail.com>
2022-10-19 14:12:46 -0400

    Started getAllSongs functionality

--- src/App.js
+++ src/App.js
@@ -3,21 +3,49 @@ import axios from 'axios';
 
 function App() {
 
-  const [song, setSongs] = useState([]);
+  const [entries, setEntries] = useState([{title: 'So Much To Say', artist: 'Dave Matthews Band', album: 'Crash', release_date: '1996-04-30', genre: 'Rock'},]);
 
-  useEffect(() =>{
-    getAllSongs();
-  }, []);
+  // const [song, setSongs] = useState([]);
 
-  async function getAllSongs(){
-    const response = await axios.get('http://127.0.0.1:8000/api/music/');
-    console.log(response.data);
-    setSongs(response.data)
-  }
+  // useEffect(() =>{
+  //   getAllSongs();
+  // }, []);
 
+  // async function getAllSongs(){
+  //   const response = await axios.get('http://127.0.0.1:8000/api/music/');
+  //   console.log(response.data);
+  //   setSongs(response.data)
+  // }
+
+  {/* <button type='submit' onClick={() => getAllSongs()}>Get All Songs</button> */}
   return (
     <div>
-      <button type='submit' onClick={() => getAllSongs()}>Get All Songs</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Entry Number</th>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Release_Date</th>
+            <th>Genre</th>
+          </tr>
+        </thead>
+        <tbody>
+          {entries.map((entry, index) => {
+            return (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{entry.title}</td>
+                <td>{entry.artist}</td>
+                <td>{entry.album}</td>
+                <td>{entry.release_date}</td>
+                <td>{entry.genre}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
